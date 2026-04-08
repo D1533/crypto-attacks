70304c8087280d3679af3307a4ed92127359187b
D1533 <davidmanresa94@gmail.com>
2026-04-08 18:22:58 +0200

    rsa small exponent attack

--- rsa/small_exponent_attack.py
+++ rsa/small_exponent_attack.py
@@ -1,31 +1,47 @@
 #!/usr/bin/env python3
 
-from Crypto.Util.number import getPrime
+from os import urandom
+from Crypto.Util.number import getPrime, GCD, bytes_to_long
 from gmpy2 import iroot
-from random import randint
+
+class VulnerableRSA:
+    def __init__(self):
+        self.e = 3
+        self.p, self.q = self.generate_primes()
+        self.n = self.p * self.q
+        self.d = pow(self.e, -1, (self.p-1)*(self.q-1))
+
+    def generate_primes(self):
+        while True:
+            p = getPrime(512)
+            q = getPrime(512)
+            if GCD(self.e, (p-1)*(q-1)) == 1:
+                return p, q
+    
+    def get_public_key(self):
+        return self.n , self.e
+
+    def encrypt(self, m):
+        return pow(m, self.e, self.n)
 
 def small_exponent_attack(c, e):
     m, exact = iroot(c, e)
     if exact:
         return m
-
     return None
 
-# --- Setup ---
-p = getPrime(512)
-q = getPrime(512)
-N = p*q
-e = 3
 
-m = randint(2, iroot(N, e)[0])
-assert(m**e < N)
-
-c = pow(m, e, N)
-
-# --- Poc - Small Exponent Attack
-m_recovered = small_exponent_attack(c, e)
-assert(m_recovered == m)
+def main():
+    # --- Setup ---
+    vuln_rsa = VulnerableRSA()
+    m = bytes_to_long(urandom(32))
+    c = vuln_rsa.encrypt(m)
+    
+    # --- Poc - Small Exponent Attack
+    n, e = vuln_rsa.get_public_key()
+    assert(m == small_exponent_attack(c, e))
 
 
 
-
+if __name__ == "__main__":
+    main()
