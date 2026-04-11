bb526ea43b3e8636eed5e6ad6b51c002ef1808be
D1533 <davidmanresa94@gmail.com>
2026-04-11 12:00:36 +0200

    rsa batch gcd attack

--- rsa/batch_gcd_attack.py
+++ rsa/batch_gcd_attack.py
@@ -3,15 +3,34 @@
 import os
 from math import gcd
 from Crypto.Util.number import bytes_to_long, getPrime
+import random
 
-def generate_rsa_key(e):
-    while True:
+class VulnerableRSA:
+    def __init__(self):
+        self.e = 0x10001
+        self.keys = self.generate_keys()
+
+    def generate_keys(self):
+        keys = []
         p = getPrime(512)
-        q = getPrime(512)
-        phi = (p-1)*(q-1) 
-        if gcd(e, phi) == 1:
-            d = pow(e, -1, phi)
-            return p, q, d
+        for _ in range(2):
+            while True:
+                q = getPrime(512)
+                if gcd(self.e, (p-1)*(q-1)) == 1:
+                    keys.append( (p*q, pow(self.e, -1, (p-1)*(q-1))))
+                    break
+        return keys
+
+    def encrypt(self, m):
+        n = random.choice(self.keys)[0]
+        return n, self.e, pow(m, self.e, n)
+    
+    def decrypt(self, c, n):
+        for n_i, d_i in self.keys:
+            if n_i == n:
+                return pow(c, d_i, n_i)
+        return None
+
 
 def batch_gcd_attack(ciphertexts, moduli, e):
     for i in range(len(ciphertexts)-1):
@@ -19,7 +38,7 @@ def batch_gcd_attack(ciphertexts, moduli, e):
             N_i = moduli[i]
             N_j = moduli[j]
             p = gcd(N_i, N_j)
-            if p != 1:
+            if p != 1 and p != N_i:
                 q = N_i // p
                 phi = (p-1)*(q-1)
                 d = pow(e, -1, phi)
@@ -27,24 +46,21 @@ def batch_gcd_attack(ciphertexts, moduli, e):
                 return m
     return None
 
-# --- Setup ---
-e = 0x10001
-m = bytes_to_long(os.urandom(32))
-ciphertexts = []
-moduli = []
-for _ in range(15):
-    p, q, d = generate_rsa_key(e)
-    N = p*q
-    moduli.append(N)
-    ciphertexts.append(pow(m, e, N))
+def main():
+    # --- Setup ---
+    vuln_rsa = VulnerableRSA()
 
-# add a public key with shared factors
-q = getPrime(512)
-N = p*q
-moduli.append(N)
-ciphertexts.append(pow(m, e, N))
+    m = bytes_to_long(os.urandom(32))
+    ciphertexts = []
+    moduli = []
+    for _ in range(100):
+        n, e, c = vuln_rsa.encrypt(m)
+        moduli.append(n)
+        ciphertexts.append(c)
 
-# --- Batch GCD Attack ---
-m_recovered = batch_gcd_attack(ciphertexts, moduli, e)
-assert(m_recovered == m)
+    # --- Batch GCD Attack ---
+    assert(m == batch_gcd_attack(ciphertexts, moduli, e))
 
+
+if __name__ == "__main__":
+    main()
