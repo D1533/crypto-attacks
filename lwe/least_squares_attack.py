e6d5298fe151c2b48f00859e49095ccb17125a2f
D1533 <davidmanresa94@gmail.com>
2026-04-11 14:49:04 +0200

    lwe least squares attack

--- lwe/least_squares_attack.py
+++ lwe/least_squares_attack.py
@@ -2,26 +2,37 @@
 
 from sage.all import *
 
-def no_modulus(A, b):
+class VulnerableLWE:
+    def __init__(self):
+        self.n = 32
+        self.m = 32
+        self.q = random_prime(2**16)
+        self.s = vector([randint(0, self.q-1) for _ in range(self.n)])
+
+    def encrypt(self):
+        A = []
+        b = []
+        for _ in range(self.m):
+            a = vector([randint(0, self.q - 1) for _ in range(self.n)])
+            A.append(a)
+            b.append(a*self.s + randint(-3,3))
+
+        return A, b, self.q
+
+def least_squares_attack(A, b):
     A = Matrix(RR, A)
     b = vector(RR, b)
     s = (A.T * A).solve_right(A.T * b)
     s = vector(ZZ, [round(s_i) for s_i in s])
     return s
 
-# --- Setup ---
-n = 32
-q = random_prime(2**16)
-s = vector([randint(0, q-1) for _ in range(n)])
-A = []
-b = []
-for i in range(n):
-    a_i = vector([randint(0,q-1) for _ in range(n)])
-    b_i = a_i*s + randint(-3, 3)
-    A.append(a_i)
-    b.append(b_i)
+def main():
+    # --- Setup ---
+    vuln_lwe = VulnerableLWE()
+    A, b, q = vuln_lwe.encrypt()
 
+    # --- PoC - Least-squares attack 
+    assert(vuln_lwe.s == least_squares_attack(A, b))
 
-# --- PoC - Least-squares attack 
-s_recovered = no_modulus(A, b)
-assert(s_recovered == s)
+if __name__ == "__main__":
+    main()
